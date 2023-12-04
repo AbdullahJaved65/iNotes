94926684140c86c60c8a1cd5735885acf5eabb31
Abdullah Javed <abdullahjaved65@yahoo.com>
2023-12-04 15:18:17 +0500

    Create a conncetion to the DB and initialize the CRUD operations.

---
+++ config.php
@@ -0,0 +1,20 @@
+<?php
+// FILEPATH: /C:/laragon/www/websites/iNotes/config.php
+
+// Database configuration
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "crud-app-php";
+
+// Create a connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . mysqli_connect_error());
+} else {
+    // Connection successful
+    echo "Connected to the database successfully!";
+}
+?>
